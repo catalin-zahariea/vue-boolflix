eccb3dbf295dd6245667057299cdb0de8d6ca40a
CATALIN ZAHARIEA <zahariea.c@gmail.com>
2021-04-29 16:45:57 +0200

    Major changes to first part.

--- js/script.js
+++ js/script.js
@@ -6,27 +6,29 @@ var app = new Vue({
   },
   methods: {
     search() {
-      console.log(this.searchInput);
+      // ---
+      // Copying array of objects from API inside searchOutput array on click if searchInput is NOT empty.
+      // ---
+      if (this.searchInput != "") {
+        // ---
+        // Getting results from the API
+        // ---
+        axios
+          .get(
+            `https://api.themoviedb.org/3/search/multi?api_key=3a6caca7bf93b48956ef86189295e101&language=en-US&page=1&include_adult=false&query=${this.searchInput}`
+          )
+          .then((response) => {
+            const res = response.data.results;
+            this.searchOutput = res;
+          });
+      }
+      // ---
+      // Resetting searchOutput array if searchInput is empty.
+      // ---
+      if (this.searchInput == "") {
+        this.searchOutput = "";
+      }
     },
   },
-  updated() {
-    // ---
-    // Getting results from the API
-    // ---
-
-    if (this.searchInput != "") {
-      axios
-        .get(
-          `https://api.themoviedb.org/3/search/multi?api_key=3a6caca7bf93b48956ef86189295e101&language=en-US&page=1&include_adult=false&query=${this.searchInput}`
-        )
-        .then((response) => {
-          const res = response.data.results;
-          this.searchOutput = res;
-        });
-    }
-
-    if (this.searchInput == "") {
-      this.searchOutput = "";
-    }
-  },
+  mounted() {},
 });
