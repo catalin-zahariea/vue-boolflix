d958c18d67de404be5944900244569852bcf6d73
CATALIN ZAHARIEA <zahariea.c@gmail.com>
2021-04-29 15:29:26 +0200

    Updates

--- js/script.js
+++ js/script.js
@@ -13,13 +13,20 @@ var app = new Vue({
     // ---
     // Getting results from the API
     // ---
-    axios
-      .get(
-        `https://api.themoviedb.org/3/search/multi?api_key=3a6caca7bf93b48956ef86189295e101&language=en-US&page=1&include_adult=false&query=${this.searchInput}`
-      )
-      .then((response) => {
-        const res = response.data.results;
-        this.searchOutput = res;
-      });
+
+    if (this.searchInput != "") {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/multi?api_key=3a6caca7bf93b48956ef86189295e101&language=en-US&page=1&include_adult=false&query=${this.searchInput}`
+        )
+        .then((response) => {
+          const res = response.data.results;
+          this.searchOutput = res;
+        });
+    }
+
+    if (this.searchInput == "") {
+      this.searchOutput = "";
+    }
   },
 });
