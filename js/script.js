91a56f6c14ab54fdf375bdb8b838474e8cd7d38f
CATALIN ZAHARIEA <zahariea.c@gmail.com>
2021-05-03 11:58:01 +0200

    Updates.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var app = new Vue({
   data: {
     searchInput: "",
     searchOutput: [],
+    searchNoResult: "",
   },
   methods: {
     search() {
@@ -27,19 +28,35 @@ var app = new Vue({
             const res = response.data.results;
             this.searchOutput = res;
             console.log(this.searchOutput);
+            // ---
+            // Filtering out objects with media_type == "person" from searchOutput array.
+            // searchOut array will contain only objects with media_type == "movie" || media_type == "tv".
+            // ---
             this.searchOutput = this.searchOutput.filter(
               (element) => element.media_type != "person"
             );
             console.log(this.searchOutput);
           });
       }
+      // ---
+      // Emptying searchOutput array when searchInput length == 0.
+      // ---
       if (this.searchInput.length == 0) {
         this.searchOutput = "";
       }
       // ---
-      // Resetting searchOutput array if searchInput is empty.
+      // Displaying error message when searchInput >= 1 and searchOutput is empty.
       // ---
+      if (this.searchOutput.length == 0 && this.searchInput.length >= 1) {
+        this.searchNoResult = "Non abbiamo trovato quello che cercavi";
+      } else {
+        // ---
+        // Resetting error message when searchInput == 0.
+        // ---
+        this.searchNoResult = "";
+      }
     },
+
     overviewItemSlice(index) {
       let overviewItemSliced = this.searchOutput[index].overview;
       overviewItemSliced = overviewItemSliced.slice(0, 70);
