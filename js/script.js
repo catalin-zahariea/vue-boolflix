a5fba87ec57d46e0803625bc28afff68508aba40
CATALIN ZAHARIEA <zahariea.c@gmail.com>
2021-05-03 10:00:31 +0200

    Major update.

--- js/script.js
+++ js/script.js
@@ -9,25 +9,36 @@ var app = new Vue({
       // ---
       // Copying array of objects from API inside searchOutput array on click if searchInput is NOT empty.
       // ---
-      if (this.searchInput != "") {
+      if (this.searchInput.length > 0) {
         // ---
         // Getting results from the API
         // ---
         axios
-          .get(
-            `https://api.themoviedb.org/3/search/multi?api_key=3a6caca7bf93b48956ef86189295e101&language=en-US&page=1&include_adult=false&query=${this.searchInput}`
-          )
+          .get("https://api.themoviedb.org/3/search/multi", {
+            params: {
+              api_key: "3a6caca7bf93b48956ef86189295e101",
+              language: "it-IT",
+              page: "1",
+              include_adult: "false",
+              query: this.searchInput,
+            },
+          })
           .then((response) => {
             const res = response.data.results;
             this.searchOutput = res;
+            console.log(this.searchOutput);
+            this.searchOutput = this.searchOutput.filter(
+              (element) => element.media_type != "person"
+            );
+            console.log(this.searchOutput);
           });
+      }
+      if (this.searchInput.length == 0) {
+        this.searchOutput = "";
       }
       // ---
       // Resetting searchOutput array if searchInput is empty.
       // ---
-      if (this.searchInput == "") {
-        this.searchOutput = "";
-      }
     },
   },
   mounted() {},
